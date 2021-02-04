289050ee482f1fa155b07b137935131854c8e316
argett <lilian.favre.garcia@efrei.net>
2021-02-04 19:28:27 +0100

    Sources basis

---
+++ Sources/Projet Analyse d'image - parking.cpp
@@ -0,0 +1,31 @@
+// Projet Analyse d'image - parking.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+//
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+    Mat image = Mat::zeros(300, 600, CV_8UC3);
+    circle(image, Point(250, 150), 100, Scalar(0, 255, 128), -100);
+    circle(image, Point(350, 150), 100, Scalar(255, 255, 255), -100);
+    imshow("Display Window", image);
+    waitKey(0);
+    return 0;
+}
+
+// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
+// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
+
+// Astuces pour bien démarrer : 
+//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
+//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
+//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
+//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
+//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
+//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
