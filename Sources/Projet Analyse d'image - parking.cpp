8f970d8b983050a46f1760f68b2bbf9b6fdd0318
argett <lilian.favre.garcia@efrei.net>
2021-02-09 23:45:21 +0100

    Changed main of 2nd file to function with namespace

--- Sources/Projet Analyse d'image - parking.cpp
+++ Sources/Projet Analyse d'image - parking.cpp
@@ -9,44 +9,46 @@
 using namespace cv;
 using namespace std;
 
-
-int main()
+namespace creationLines 
 {
-    char wndname[] = "Drawing Demo";
-    const int DELAY = 5;
-    int lineType = LINE_AA; // change it to LINE_8 to see non-antialiased graphics
-    int i, width = 600, height = 300;
-    int x1 = -width / 2, x2 = width * 3 / 2, y1 = -height / 2, y2 = height * 3 / 2;
-    RNG rng(0xFFFFFFFF);
-
-    Mat image = Mat::zeros(height, width, CV_8UC3);
-    int r = 100;
-    int v = 100;
-    for (i = 0; i < 5; i++)
+    void create_lines()
     {
-        Point pt1, pt2;
-        pt1.x = rng.uniform(x1, x2);
-        pt1.y = rng.uniform(y1, y2);
-        pt2.x = rng.uniform(x1, x2);
-        pt2.y = rng.uniform(y1, y2);
+        char wndname[] = "Drawing Demo";
+        const int DELAY = 5;
+        int lineType = LINE_AA; // change it to LINE_8 to see non-antialiased graphics
+        int i, width = 600, height = 300;
+        int x1 = -width / 2, x2 = width * 3 / 2, y1 = -height / 2, y2 = height * 3 / 2;
+        RNG rng(0xFFFFFFFF);
 
-        line(image, pt1, pt2, Scalar(r, v, 255), rng.uniform(1, 1), lineType);
-        r += 100;
-        if (r > 255) {
-            r = 255;
-            v += 100;
-            if (v > 255)
-                v = 255;
+        Mat image = Mat::zeros(height, width, CV_8UC3);
+        int r = 100;
+        int v = 100;
+        for (i = 0; i < 5; i++)
+        {
+            Point pt1, pt2;
+            pt1.x = rng.uniform(x1, x2);
+            pt1.y = rng.uniform(y1, y2);
+            pt2.x = rng.uniform(x1, x2);
+            pt2.y = rng.uniform(y1, y2);
+
+            line(image, pt1, pt2, Scalar(r, v, 255), rng.uniform(1, 1), lineType);
+            r += 100;
+            if (r > 255) {
+                r = 255;
+                v += 100;
+                if (v > 255)
+                    v = 255;
+            }
         }
+
+        imshow("Display Window", image);
+
+        //HoughLines() A FAIRE EN PRENANT LE FICHIER DE JULIE 
+
+        waitKey(0);
     }
-
-    imshow("Display Window", image);
-
-    //HoughLines() A FAIRE EN PRENANT LE FICHIER DE JULIE 
-
-    waitKey(0);
-    return 0;
 }
+
 
 // Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
 // Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
