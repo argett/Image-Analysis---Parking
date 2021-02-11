1687131b4ceb4be4ad5f3d8c7994a530baefc0d0
argett <lilian.favre.garcia@efrei.net>
2021-02-11 16:54:22 +0100

    Added fit lines, removed random white pixels

--- Sources/First detection of lines.cpp
+++ Sources/First detection of lines.cpp
@@ -10,7 +10,7 @@ using namespace std;
 int main()
 {
     //read the image file
-    Mat img = imread("./Images/parking1.jpg");
+    Mat img = imread("./Images/parking3.jpg");
 
     if (img.empty()) // Check for failure
     {
@@ -18,34 +18,64 @@ int main()
         system("pause"); //wait for any key press
         return -1;
     }
-
+    
     // Convert to gray-scale
-    Mat greyMat, colorMat;
+    Mat greyMat;
     cvtColor(img, greyMat, cv::COLOR_RGB2GRAY);
 
     // Store the edges   
-    Mat edges;
+    Mat FirstEdges;
 
     // Find the edges in the image using canny detector
-    Canny(greyMat, edges, 200, 255);
-    //cout << edges << endl;
+    Canny(greyMat, FirstEdges, 200, 255);
+
     // Create a vector to store lines of the image
-    vector<Vec4i> lines;
-    
-    // Apply Hough Transform
-    HoughLinesP(edges, lines, 1, CV_PI / 180, 100, 20, 20);
-    for (size_t i = 0; i < lines.size(); i++) {
+    vector<Vec4i> FirstLines;
 
-        Vec4i l = lines[i];
+    // Apply First Hough Transform
+    HoughLinesP(FirstEdges, FirstLines, 1, CV_PI / 180, 100, 20, 20);
+    for (size_t i = 0; i < FirstLines.size(); i++) {
 
+        Vec4i l = FirstLines[i];
+        
         line(greyMat, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(255, 0, 0), 3, LINE_AA);
     }
-    imshow("Image parking", greyMat);
+
+    // image in black and white only with the lines detected + random white pixels
+    Mat BW_mat;
+    // to erase the cars and keep only the whites lines + random white pixels
+    threshold(greyMat, BW_mat, 254, 255, THRESH_BINARY);
+    // to erase random white pixels
+    erode(BW_mat, BW_mat, Mat());
+    // to get beautiful lines
+    dilate(BW_mat, BW_mat, Mat(), Point(-1,-1),3);
+
+
+    // Store the edges   
+    Mat SecondEdges;
+    // Create a vector to store lines of the image
+    vector<Vec4i> SecondLines;
+
+    // Apply Second Hough Transform
+    HoughLinesP(SecondEdges, SecondLines, 1, CV_PI / 180, 100, 20, 20);
+    for (size_t i = 0; i < SecondLines.size(); i++) {
+
+        Vec4i l = SecondLines[i];
+
+        line(BW_mat, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(255, 0, 0), 3, LINE_AA);
+        circle(BW_mat, Point(l[0], l[1]), 2, Scalar(255,0,0), 10);
+        circle(BW_mat, Point(l[2], l[3]), 2, Scalar(255,0,0), 10);
+    }
+
+    imshow("img", img);
+    imshow("GREY", greyMat);
+    imshow("Image parking", BW_mat);
         
 
 
     //imshow("Mon Parking Jacky", img);
-    waitKey(0); // Wait for any keystroke in the window
 
+    
+    waitKey(0); // Wait for any keystroke in the window
     return 0;
 }
