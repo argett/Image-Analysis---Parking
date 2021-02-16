8896f6a91414dbb16d153f82a916ce5a65a7660c
argett <lilian.favre.garcia@efrei.net>
2021-02-16 19:49:40 +0100

    Changed variable name

--- Sources/First detection of lines.cpp
+++ Sources/First detection of lines.cpp
@@ -28,7 +28,7 @@ int  dist(int x, int y)
 int main()
 {
     //read the image file
-    Mat img = imread("./Images/parking1.jpg");
+    Mat img = imread("./Images/parking3.jpg");
 
     if (img.empty()) // Check for failure
     {
@@ -95,7 +95,6 @@ int main()
         Vec4i l = SecondLines[j];
         line(BW_mat, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(255, 0, 0), 3, LINE_AA);
 
-
         circle(BW_mat, Point(l[0], l[1]), 2, Scalar(200), 10);
         circle(BW_mat, Point(l[2], l[3]), 2, Scalar(100, 255, 255), 10);
     }
@@ -105,40 +104,42 @@ int main()
     bool Pd_confondue; 
     bool Pf_confondue;
     bool PdPf_confondue;
-
+    
     // check the points between them to keep only one by line
-    for (int u = 0; u < SecondLines.size(); u++) {
+    for (size_t u = 0; u < SecondLines.size(); u++) {
 
-        for (int v = u; v < SecondLines.size(); v++)
+        for (size_t v = u; v < SecondLines.size(); v++)
         {
+            
             Pd_confondue = false;
             Pf_confondue = false;
             PdPf_confondue = false; 
+            
+            Vec4i ligne = SecondLines[u];
+            Vec4i ligne2 = SecondLines[v];
 
-            Vec4i l = SecondLines[u];
-            Vec4i l2 = SecondLines[v];
 
             int marge_erreur = 20;
 
             //point x depart ligne 1
-            int Pdx1 = l[0];
-            //point y dï¿½part ligne 1
-            int Pdy1 = l[1];
+            int Pdx1 = ligne[0];
+            //point y départ ligne 1
+            int Pdy1 = ligne[1];
             //point x fin ligne 1
             int Pfx1 = ligne[2];
             //point y fin ligne 1
             int Pfy1 = ligne[3];
 
             //point x depart ligne 2
-            int Pdx2 = l2[0];
-            //point y dï¿½part ligne 2
-            int Pdy2 = l2[1];
+            int Pdx2 = ligne2[0];
+            //point y départ ligne 2
+            int Pdy2 = ligne2[1];
             //point x fin ligne 2
             int Pfx2 = ligne2[2];
             //point y fin ligne 2
             int Pfy2 = ligne2[3];
 
-
+            
             if (dist(Pdx1, Pdx2) < marge_erreur && dist(Pdy1, Pdy2) < marge_erreur)
                 Pd_confondue = true;
              
@@ -153,13 +154,13 @@ int main()
             if (Pd_confondue && !Pf_confondue)
             {
                 //TODO
-                // 1 check s'ils sont collinï¿½aire + longeur du segment 
+                // 1 check s'ils sont collinéaire + longeur du segment 
                 // 2 si collineaire prendre le plus grand 
             }
             else if (!Pd_confondue && Pf_confondue)
             {
                 //TODO
-                //1 check s'ils sont collinï¿½aire + longeur du segment 
+                //1 check s'ils sont collinéaire + longeur du segment 
                 // 2 si collineaire prendre le plus grand 
             }
             else if (Pd_confondue && Pf_confondue)
@@ -171,17 +172,17 @@ int main()
             else if (PdPf_confondue)
             {
                 //TODO
-                // 1 Si collinï¿½aire 
-                // 2 vï¿½rifier les points confondues et creer un nouveau vecteur avec les point restant 
+                // 1 Si collinéaire 
+                // 2 vérifier les points confondues et creer un nouveau vecteur avec les point restant 
 
-                //Sinon rien car supp dans les autres itï¿½rations
+                //Sinon rien car supp dans les autres itérations
             }
             else 
             {
                 cout << "OUII NTM" << endl;
             }
+            
         }
-
 
         //Vec4i l = SecondLines[j];
         //line(BW_mat, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(255, 0, 0), 3, LINE_AA);
